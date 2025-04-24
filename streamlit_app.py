4b4a42bfb6bf1307bf3947a7aed04b123502f92a
pupa03 <pupatales@hotmail.com>
2025-04-24 14:45:19 +0700

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,20 @@ st.write(
 
 st.write('Test pls')
 st.write('Help pls')
+
+import streamlit as st
+
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
+
+if genre == ":rainbow[Comedy]":
+    st.write("You selected comedy.")
+else:
+    st.write("You didn't select comedy.")
